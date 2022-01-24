e968e84596bfe34a57f778a65b4dc96b23e2dd8b
Oluwatobiloba Ogunbi <tobiogunbi@gmail.com>
2022-01-24 10:52:08 -0500

    math

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer Sum", intSum)
 
-	fmt.Println("Math")
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float Sum:", floatSum)
+
+	floatSum = math.Round(floatSum*100) / 100
+	fmt.Println("The float is now:", floatSum)
+
+	circleRadius := 15.5
+	circumference := circleRadius * 2 * math.Pi
+	fmt.Printf("Circumference: %.2f\n", circumference)
 
 }
